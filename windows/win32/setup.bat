da779bd62408d20af34506286de1d94e6d38797c
liulei <mytliulei@gmail.com>
2015-05-20 16:32:54 +0800

    move setup2 to setup1 and add ipv6 install

--- windows/win32/setup.bat
+++ windows/win32/setup.bat
@@ -54,8 +54,62 @@ exit )
 13-pycrypto-2.6.win32-py2.7.exe
 @echo 安装pycrypto完成
 
-@echo 请将[盘符]:\Python27;[盘符]:\Python27\Scripts;加入环境变量Path后再按任意键
+@echo 请将[盘符]:\Python27;[盘符]:\Python27\Scripts;加入环境变量Path后再按任意键继续
 
 pause
 
-setup2.bat
+@echo 开始安装robotframework2.8.6
+04-robotframework-2.8.6.win32.exe
+@echo 安装robotframework2.8.6完成
+
+@echo 开始安装ride-1.3
+05-robotframework-ride-1.3.win32.exe
+@echo 安装ride-1.3完成
+
+@echo 如果05-robotframework-ride-1.3.win32.exe没有安装成功，请手动安装，安装成功后，桌面会有机器人的快捷图标
+
+@echo 安装ride成功后，请按任意键继续安装其他依赖包
+
+pause
+
+@echo 开始安装pyte
+cd pyte-0.4.8
+python setup.py install
+cd ..
+
+@echo 安装ecdsa
+cd ecdsa-0.11
+python setup.py install
+cd ..
+
+@echo 安装paramiko
+cd paramiko-1.14.0
+python setup.py install
+cd ..
+
+@echo 安装robotframework-sshlibrary
+cd robotframework-sshlibrary-2.0.2
+python setup.py install
+cd ..
+
+@echo 安装scapy
+cd scapy-2
+python setup.py install
+cd ..
+
+@echo 安装pygal
+cd pygal-1.7.0
+python setup.py install
+cd ..
+
+@echo on
+
+@echo RobotFramework安装完成，按任意键继续安装ipv6组件，win7及以上已自带，报错请无视 
+
+pause
+
+ipv6 install
+
+@echo ipv6组件安装完毕，按任意键结束安装
+
+pause
--- windows/win32/setup2.bat
+++ windows/win32/setup2.bat
@@ -1,12 +1,4 @@
 
-@echo 开始安装robotframework2.8.6
-04-robotframework-2.8.6.win32.exe
-@echo 安装robotframework2.8.6完成
-
-@echo 开始安装ride-1.3
-05-robotframework-ride-1.3.win32.exe
-@echo 安装ride-1.3完成
-
 @echo 开始安装pyte
 cd pyte-0.4.8
 python setup.py install
